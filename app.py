4fa3a3b4ed74348ecea3f097a0abfc1c74d53a54
Javier Ruiz <jruizard@uwaterloo.ca>
2017-09-06 00:08:56 -0400

    Fixed missing paranthesis on print

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ import requests
 app = Flask(__name__)
 
 # Page Access Token from FB
-PAT = ''
+PAT = 'EAAGojsu8t7UBAIX7BaQkj5rWvyNvXWoRP8coZBWUN81VxQaOyl7HTIiV4ZBYQQav5fXFwlE8LSH56vRcqZAvGTcKerVVf010hqlotVzUpGh38Jt4eTwte5zIvza6pwKeIwa1Ut2AOYnloLirLPLoZCDvA2bNElrpd24X5oF9qQZDZD'
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-    print "Handling Verification."
+    print("Handling Verification.")
     if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
-        print "Verification successful."
+        print("Verification successful.")
         return request.args.get('hub.challenge', '')
     else:
-        print "Verification failed."
+        print("Verification failed.")
         return "Error, wrong validation token"
 
 @app.route('/', methods=['POST'])
 def handle_messages():
-    print "Handling Messages."
+    print("Handling Messages.")
     payload = request.get_data()
-    print payload
+    print(payload)
     for sender, message in messaging_events(payload):
-        print "Incoming from %s, %s" % (sender, message)
+        print("Incoming from %s, %s" % (sender, message))
         send_message(PAT, sender, message)
     return "OK"
 
@@ -49,7 +49,7 @@ def send_message(token, recipient, text):
         }),
         headers={'Content-type': 'application/json'})
     if r.status_code != requests.codes.ok:
-        print r.text
+        print(r.text)
 
 if __name__ == '__main__':
     app.run()
